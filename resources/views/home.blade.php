8cd80ce32eaae06429fdce2d909876a9700e1b01
Edwin Smith <edwin@showit.co>
2018-01-20 16:09:21 -0700

    integration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purchase North Wind | Author Michael Hale</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+<div class="top-bar">
+    <div class="top-bar-right">
+        <ul class="menu" data-smooth-scroll>
+            <li><a href="#the-book">The Book</a></li>
+            <li><a href="#the-author">The Author</a></li>
+            <li class="menu-purchase-link"><a href="#purchase">Purchase</a></li>
+        </ul>
+    </div>
+</div>
+<section class="banner-hero">
+    <img class="hero-image" src="images/banner.jpg" alt="">
+    <div class="burst-container">
+        <div id="burst"></div>
+        <div class="burst-content">
+            <a href="#purchase">Available<br>for Purchase<br>Now!</a>
+        </div>
+    </div>
+</section>
+<section id="endorsement">
+    <div class="grid-x align-center">
+        <div class="cell medium-8">
+            <blockquote>
+                “A fresh new voice in thriller fiction, Michael Hale’s <strong>North Wind</strong> is ripped from today’s headlines, visceral and compelling. Hale is the real deal!”
+                <cite>Steve Alten, New York Times best selling author of Meg and Undisclosed.</cite>
+            </blockquote>
+        </div>
+    </div>
+</section>
+<section id="purchase">
+    <div class="grid-x align-center align-middle">
+        <div class="cell medium-4">
+            <img src="images/mockup.png" alt="">
+        </div>
+        <div class="cell medium-6">
+            <form action="">
+                <div class="grid-x grid-padding-x">
+                    <div class="cell">
+                        <h2>Purchase <strong>North Wind</strong></h2>
+                    </div>
+                    <div class="cell">
+                        <label for="name">Enter your name
+                            <input type="text" name="name">
+                        </label>
+                    </div>
+                    <div class="cell">
+                        <label for="address">Mailing address
+                            <input type="text" name="address">
+                        </label>
+                    </div>
+                    <div class="cell small-12 medium-6">
+                        <label for="city">City
+                            <input type="text" name="city">
+                        </label>
+                    </div>
+                    <div class="cell small-12 medium-3">
+                        <label for="city">State
+                            <input type="text" name="state">
+                        </label>
+                    </div>
+                    <div class="cell small-12 medium-3">
+                        <label for="zip">State
+                            <input type="text" name="zip">
+                        </label>
+                    </div>
+                    <div class="cell">
+                        <label for="card">Card
+                            <input type="text" name="card">
+                        </label>
+                    </div>
+                    <div class="cell small-12 medium-6">
+                        <label for="expiration">Expiration
+                            <input type="text" name="expiration">
+                        </label>
+                    </div>
+                    <div class="cell small-12 medium-6">
+                        <label for="cvc">CVC
+                            <input type="text" name="cvc">
+                        </label>
+                    </div>
+                    <div class="cell">
+                        <input type="submit" name="submit" value="Pay $25" class="button primary">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+<section id="the-book">
+    <div class="grid-x align-center">
+        <div class="medium-8 cell">
+            <h2>From the <strong>book</strong></h2>
+            <p>The men scanned the clear star filled sky. An eerie silence had spread across the area, as if nature had pulled the covers up over her head for a long night’s sleep. The loons no longer yodeled, and the crickets had ceased their nightly chatter.</p>
+            <p>“This is weird,” Brice said. “I can’t quite…”, he stopped short and looked at his feet.</p>
+            <p>The ground had begun to vibrate, sending tingling sensations up their legs.</p>
+            <p>“Holy shit,” William exclaimed. “Look at that!”</p>
+            <p>High up in the atmosphere, hundreds of fireballs were streaking across the sky heading north and north east, each leaving an arcing tracer in its wake. As they watched the light snow unfold it slowly formed a web like appearance as the beams intertwined. A high-pitched buzzing sound rang in their ears.</p>
+            <p>“Since when do meteors make noise, unless they’re getting ready to crash into your house?” John asked curiously.</p>
+            <p>Brice shook his head and declared loudly, “They’re not meteors!"</p>
+        </div>
+    </div>
+</section>
+<section id="the-author" class="about-the-author">
+    <div class="grid-x grid-padding-x align-center">
+        <div class="cell medium-6">
+            <h2 class="author-title">About the <strong>Author</strong></h2>
+            <h3 class="separator-left">Michael Hale</h3>
+            <p>
+                I am an avid fisherman and have taken yearly fly-in fishing trips into the Canadian wilderness since the mid seventies. The harsh reality is that while we are in the bush events occur in the world without our knowledge. In 1997, on the last day of our trip, the pilot told us of Princess Diana’s death as we loaded the plane. That triggered the question... ‘What if we were never picked up?’ From that day forward North Wind had begun its development.
+            </p>
+            <p>
+                I turned sixty years old in 2017 and have been married to Sharon for 32 years. We have 4 wonderful children and 8 grandchildren with another on the way. This is my first novel and I am excited to share it with all of you! Six years of my life have gone into its publication, and I sincerely hope you enjoy every page. Thank you for taking part in my adventure.
+            </p>
+        </div>
+        <div class="cell medium-4">
+            <div class="author-image">
+                <img src="images/headshot.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<div class="top-bar bottom-bar align-center">
+    <ul class="menu">
+        <li class="menu-text">Copyright Michael Hale 2018 &copy;</li>
+    </ul>
+</div>
+<script src="bower_components/jquery/dist/jquery.js"></script>
+<script src="bower_components/what-input/dist/what-input.js"></script>
+<script src="bower_components/foundation-sites/dist/js/foundation.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
